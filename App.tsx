510973bc42ad6c66198a327a0880e947013c2487
Crislâine Santos <criis.santos@outlook.com>
2022-04-18 17:49:32 -0300

    feat: add Login screen and loaded fonts

--- App.tsx
+++ App.tsx
@@ -1,20 +1,32 @@
+import AppLoading from 'expo-app-loading';
+import { useFonts } from 'expo-font';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { ThemeProvider } from 'styled-components';
+
+import { Login } from '@screens';
+
+import { theme } from '@styles';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+	const [loadedFonts] = useFonts({
+		'Helvetica-Neue-Bold': require('./assets/fonts/Helvetica-Neue-Bold.ttf'),
+		'Helvetica-Neue-Bold-Italic': require('./assets/fonts/Helvetica-Neue-Bold-Italic.ttf'),
+		'Helvetica-Neue-Italic': require('./assets/fonts/Helvetica-Neue-Italic.ttf'),
+		'Helvetica-Neue-Light': require('./assets/fonts/Helvetica-Neue-Light.ttf'),
+		'Helvetica-Neue-Light-Italic': require('./assets/fonts/Helvetica-Neue-Light-Italic.ttf'),
+		'Helvetica-Neue-Medium': require('./assets/fonts/Helvetica-Neue-Medium.ttf'),
+		'Helvetica-Neue-Regular': require('./assets/fonts/Helvetica-Neue-Regular.ttf'),
+	});
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+	if (!loadedFonts) {
+		return <AppLoading />;
+	}
+
+	return (
+		<ThemeProvider theme={theme}>
+			<StatusBar style='auto' />
+			<Login />
+		</ThemeProvider>
+	);
+}
