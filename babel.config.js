2ab7d6f422116a40fc137cc858bd0c6209355be5
Crislâine Santos <criis.santos@outlook.com>
2022-04-18 17:50:29 -0300

    build: config path maping and install dependencies

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,20 @@
-module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-  };
+module.exports = function (api) {
+	api.cache(true);
+
+	return {
+		presets: ['babel-preset-expo'],
+		plugins: [
+			[
+				'module-resolver',
+				{
+					alias: {
+						'@screens': './src/screens/index',
+						'@components': './src/components/index',
+						'@styles': './src/shared/styles/index',
+						'@types': './src/shared/types/index',
+					},
+				},
+			],
+		],
+	};
 };
