802e952fef76c2ca57f196ddf8a2572708c057b1
Crislâine Santos <criis.santos@outlook.com>
2022-04-19 22:14:40 -0300

    feat: new types added

--- src/shared/types/index.ts
+++ src/shared/types/index.ts
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { IBet, IGameType, IUser, IToken } from '@interfaces';
+
 export type FormProps = {
 	children: React.ReactNode;
 	title: string;
@@ -9,10 +11,27 @@ export type FormProps = {
 };
 
 export type ButtonProps = {
-	arrow?: string;
-	title: string;
-	onGoTo?: () => void;
-	fontSize: number;
-	color: string;
-	showIcon?: boolean;
+	children: React.ReactNode;
+	onPressHandler: () => void;
 };
+
+export type IconButtonProps = {
+	icon: any;
+	size: number;
+	color: string;
+};
+
+export type BetsSlice = {
+	bets: IBet[] | [];
+	querys: string[];
+};
+
+export type GamesSlice = {
+	list: IGameType[],
+	currentGame: IGameType | {}
+}
+
+export type UserSlice = {
+	user: IUser | {},
+	token: IToken | {}
+}
