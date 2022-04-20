3e65e10181e01d10015b8c6e57a5e3d1bb0ffa01
Crislâine Santos <criis.santos@outlook.com>
2022-04-19 22:14:21 -0300

    feat: add utils functions

---
+++ src/shared/utils/index.ts
@@ -0,0 +1,21 @@
+import { completeArray } from './completeArray';
+
+import { formatDate } from './formatDate';
+import { formatValueToCurrency } from './formatValueToCurrency';
+import { formatNumbers } from './formatNumbers';
+
+import { getGameColor } from './game/getGameColor';
+import { getGameName } from './game/getGameName';
+import { getGamePrice } from './game/getGamePrice';
+import { getGamePriceNumber } from './game/getGamePriceNumber';
+
+export {
+	completeArray,
+	formatDate,
+	formatValueToCurrency,
+	formatNumbers,
+	getGameColor,
+	getGameName,
+	getGamePrice,
+	getGamePriceNumber,
+};
